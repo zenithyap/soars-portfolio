ba9ba60c78fd26e4cbdb727709b2706bd34012ad
zenith <zenithyap88@gmail.com>
2024-10-09 21:35:15 +0800

    Change images name

--- src/components/DigitalArt.js
+++ src/components/DigitalArt.js
@@ -1,12 +1,13 @@
-import Image1 from '../Images/DigitalArt/Image1.jpg';
-import Image2 from '../Images/DigitalArt/Image2.jpg';
-import Image3 from '../Images/DigitalArt/Image3.jpg';
-import Image4 from '../Images/DigitalArt/Image4.jpg';
-import Image5 from '../Images/DigitalArt/Image5.jpg';
-import Image6 from '../Images/DigitalArt/Image6.jpg';
-import Image7 from '../Images/DigitalArt/Image7.jpg';
-import Image8 from '../Images/DigitalArt/Image8.jpg';
-import Image9 from '../Images/DigitalArt/Image9.jpg';
+import shooting_star from '../Images/DigitalArt/shooting_star.jpg';
+import cat_piano from '../Images/DigitalArt/cat_piano.jpg';
+import sun_moon from '../Images/DigitalArt/sun_moon.jpg';
+import capybara from '../Images/DigitalArt/capybara.jpg';
+import sad_fox from '../Images/DigitalArt/sad_fox.jpg';
+import campfire from '../Images/DigitalArt/campfire.jpg';
+import butterfly from '../Images/DigitalArt/butterfly.jpg';
+import souffle from '../Images/DigitalArt/souffle.jpg';
+import grandma from '../Images/DigitalArt/grandma.jpg';
+
 
 export default function DigitalArt() {
     return (
@@ -16,19 +17,19 @@ export default function DigitalArt() {
             </div>
             <div className="grid grid-cols-3 gap-8 border-24 border-transparent w-5/6">
                 <div className="grid gap-4">
-                    <img className="rounded-xl" src={Image4}/>
-                    <img className="rounded-xl" src={Image2}/>
-                    <img className="rounded-xl" src={Image6}/>
+                    <img className="rounded-xl" src={capybara} alt="capybara_image" />
+                    <img className="rounded-xl" src={cat_piano} alt="cat_piano_image" />
+                    <img className="rounded-xl" src={campfire} alt="campfire_image" />
                 </div>
                 <div className="grid gap-4">
-                    <img className="rounded-xl" src={Image1}/>
-                    <img className="rounded-xl" src={Image9}/>
-                    <img className="rounded-xl" src={Image7}/>
+                    <img className="rounded-xl" src={shooting_star} alt="shooting_star_image" />
+                    <img className="rounded-xl" src={grandma} alt="grandma_image" />
+                    <img className="rounded-xl" src={butterfly} alt="butterfly_image" />
                 </div>
                 <div className="grid gap-4">
-                    <img className="rounded-xl" src={Image5}/>
-                    <img className="rounded-xl" src={Image3}/>
-                    <img className="rounded-xl" src={Image8}/>
+                    <img className="rounded-xl" src={sad_fox} alt="sad_fox_image" />
+                    <img className="rounded-xl" src={sun_moon} alt="sun_moon_image" />
+                    <img className="rounded-xl" src={souffle} alt="souffle_image" />
                 </div>
             </div>
         </div>
