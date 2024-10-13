fac42ca70100de53ecd89d861b3dd8582a50b6c2
zenith <zenithyap88@gmail.com>
2024-10-13 16:16:24 +0800

    Change navbar font

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,7 +2,7 @@ import Dropdown from "./Dropdown";
 
 export default function NavigationBar() {
     return (
-        <div className="flex flex-row justify-center absolute z-10 w-screen select-none">
+        <div className="flex flex-row justify-center absolute z-10 w-screen select-none font-sans font-semibold">
             <div className="flex gap-10 p-5">
                 <Dropdown/>
                 <div className="p-2 hover: cursor-pointer">
