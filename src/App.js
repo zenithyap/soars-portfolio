85caecbfc6301abed69966d0f63601a193126557
zenith <zenithyap88@gmail.com>
2024-10-22 14:40:43 +0800

    Remove hash redirect

--- src/App.js
+++ src/App.js
@@ -2,21 +2,7 @@ import './App.css';
 import LandingPage from './components/LandingPage';
 import EducationPage from './components/EducationPage';
 import NavigationBar from './components/NavigationBar';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
-
-// const HashRedirect = () => {
-//   const location = useLocation();
-
-//   useEffect(() => {
-//     if (location.hash && location.pathname === "/") {
-//       window.location.href = "/"
-//     }
-//   // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, []);
-
-//   return null;
-// }
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
